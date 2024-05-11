8f7e7b059fd696ed5bd445aee32858d9b261a7f4
nadavtal <nadavtalalamgor@gmail.com>
2024-05-11 12:48:43 +0300

    uploads

--- uploads/src/routes/uploads.ts
+++ uploads/src/routes/uploads.ts
@@ -1,35 +1,33 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest } from '@ntatickets/common';
-import { Ticket } from '../models/ticket';
-import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
-import { natsWrapper } from '../nats-wrapper';
+// import { Ticket } from '../models/ticket';
+// import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+// import { natsWrapper } from '../nats-wrapper';
 const router = express.Router();
 
 router.post(
-  '/api/tickets',
-  requireAuth,
-  [
-    body('title').not().isEmpty().withMessage('Title is required'),
-    body('price')
-      .isFloat({ gt: 0 })
-      .withMessage('Price must be greater than 0'),
-  ],
-  validateRequest,
+  '/api/uploads',
+  // requireAuth,
+  // [
+  //   body('title').not().isEmpty().withMessage('Title is required'),
+  // ],
+  // validateRequest,
   async (req: Request, res: Response) => {
-    const { title, price } = req.body;
-    const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
-    await ticket.save();
-    new TicketCreatedPublisher(natsWrapper.client).publish({
-      id: ticket.id,
-      version: ticket.version,
-      title: ticket.title,
-      price: ticket.price,
-      userId: ticket.userId,
-    });
+    const { fileName, destination } = req.body;
+    console.log({ fileName, destination });
+    // const ticket = Ticket.build({ title, price, userId: req.currentUser!.id });
+    // await ticket.save();
+    // new TicketCreatedPublisher(natsWrapper.client).publish({
+    //   id: ticket.id,
+    //   version: ticket.version,
+    //   title: ticket.title,
+    //   price: ticket.price,
+    //   userId: ticket.userId,
+    // });
 
-    res.status(201).send(ticket);
+    // res.status(201).send(ticket);
   }
 );
 
-export { router as createTicketRouter };
+export { router as uploadsRouter };
